2990f4efca8e109da83d31a213e0e8e5bc234a35
Orin Fletcher <orinf@yahoo.com>
2021-01-17 12:49:49 -0800

    finishes challenge

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
-// CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [honda, tesla] = cars;
+// honda destructured data
+const { coloursByPopularity: hondaColors, speedStats: hondaSpeed } = honda;
+const [hondaTopColour] = hondaColors;
+const { topSpeed: hondaTopSpeed } = hondaSpeed;
+// tesla destructured data
+const { coloursByPopularity: teslaColors, speedStats: teslaSpeed } = tesla;
+const [teslaTopColour] = teslaColors;
+const { topSpeed: teslaTopSpeed } = teslaSpeed;
 
 ReactDOM.render(
   <table>
-    <tr>
-      <th>Brand</th>
-      <th>Top Speed</th>
-    </tr>
-    <tr>
-      <td>{tesla.model}</td>
-      <td>{teslaTopSpeed}</td>
-      <td>{teslaTopColour}</td>
-    </tr>
-    <tr>
-      <td>{honda.model}</td>
-      <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td>
-    </tr>
+    <tbody>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+        <th>Top Colour</th>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+    </tbody>
   </table>,
   document.getElementById("root")
 );
